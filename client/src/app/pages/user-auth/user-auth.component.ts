1bbe0aedb1220a1ba76ca6691c9aaac89aae37e4
Dela Ricch <delaricch@gmail.com>
2024-01-26 08:29:41 +0000

    feat: added confrim password validation

--- client/src/app/pages/user-auth/user-auth.component.ts
+++ client/src/app/pages/user-auth/user-auth.component.ts
@@ -1,32 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LogoComponent } from '../../icons/logo/logo.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { confirmPasswordValidator } from '../../../helpers';
-import { ActivatedRoute, Route } from '@angular/router';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { NgClass } from '@angular/common';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-user-auth',
   standalone: true,
-  imports: [LogoComponent, InputTextModule, ButtonModule, ReactiveFormsModule, NgClass],
+  imports: [LogoComponent, InputTextModule, ButtonModule, ReactiveFormsModule, NgClass, ProgressSpinnerModule, RouterLink],
   templateUrl: './user-auth.component.html',
   styleUrl: './user-auth.component.css'
 })
-export class UserAuthComponent {
+export class UserAuthComponent implements OnInit {
 
   isSignUp = false;
-
-  userAuthForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.pattern('[A-Za-z\d!@#$%^&*()_+]{8,}')]),
-    confirmPassword: new FormControl('', [Validators.required]),
-    termsAndConditions: new FormControl('', [Validators.required]),
-    rememberMe: new FormControl('', [Validators.required])
-  },
-   { validators: confirmPasswordValidator});
+  isSubmitting = false;
+  displayPassword = false;
+  displayConfirmPassword = false;
 
   constructor(private route: ActivatedRoute) {
     this.route.data.subscribe(data => {
@@ -34,4 +27,29 @@ export class UserAuthComponent {
     });
    }
 
+  userAuthForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)[a-zA-Z\d\W]{8,}$/)]),
+    confirmPassword: new FormControl('', [Validators.required]),
+    termsAndConditions: new FormControl('', [Validators.required]),
+    rememberMe: new FormControl('', [Validators.required])
+  },
+  {validators: this.confirmPasswordValidator}
+   );
+
+   confirmPasswordValidator(control: AbstractControl) {
+     return control.get('password')?.value === control.get('confirmPassword')?.value
+     ? null : { mismatch: true };
+   }
+
+   onSubmit(event: SubmitEvent) {
+    event.preventDefault();
+     this.isSubmitting = true;
+     console.log(this.userAuthForm.value);
+   }
+
+   ngOnInit(): void {
+       
+   }
 }
